27537f62ec9d3b7b8d1d1f2ae1047ff1adf475a8
Rmiverson <Rmiverson99@gmail.com>
2022-08-02 11:55:10 -0700

    added crud components to recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,45 @@
 class RecipesController < ApplicationController
+    def create
+        recipe = Recipe.create(recipe_params)
+        
+        if recipe.valid?
+            render json: RecipeSerializer.new(recipe).serialized_json
+        else
+            render json: { error: "Invalid inputs"}, status: => 422
+        end
+    end
+
+    def index
+        recipe = Recipe.all
+        
+        render json: RecipeSerializer.new(recipe).serialized_json
+    end
+
+    def show
+        recipe = Recipe.find(params[:id])
+
+        render json: RecipeSerializer.new(recipe).serialized_json
+    end
+
+    def update
+        recipe = Recipe.find(params[:id])
+
+        recipe.update(recipe_params)
+
+        render json: RecipeSerializer.new(recipe).serialized_json
+    end
+
+    def destroy
+        recipe = Recipe.find(params[:id])
+        recipe.destroy
+        render json: {message: "Recipe successfully deleted"}, status: 200
+    end
+
+
+
+    private
+
+    def recipe_params
+        params.require(:recipe)
+    end
 end
