0b4514a387b9d30522bd125351fd52ab1d591682
rmiverson <rmiverson99@gmail.com>
2022-08-11 00:56:11 -0700

    built the controllers for tags, textboxes, and unordered lists

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,37 @@
 class TagsController < ApplicationController
+    def create
+        tag = Tag.create(tag_params)
+
+        if tag.valid?
+            render json: {message: "Tag successfully created."}, status: 200
+        else
+            render json: {message: "Failed to create tag, invalid inputs."}, status: 422
+        end
+    end
+
+    def index
+        tags = Tag.all
+
+        render json: TagSerializer.new(tags).serialized_json
+    end
+
+    def show
+        tag = Tag.find(params[:id])
+
+        render json: TagSerializer.new(tag).serialized_json
+    end
+
+    def destroy
+        tag = Tag.find(params[:id])
+        
+        tag.destroy
+
+        render json: {message: "Tag successfully deleted."}, status: 200
+    end
+
+    private
+
+    def tag_params
+        params.require(:tag).permit(:label)
+    end
 end
--- app/controllers/textboxes_controller.rb
+++ app/controllers/textboxes_controller.rb
@@ -1,2 +1,30 @@
 class TextboxesController < ApplicationController
+    def create
+        text_box = Textbox.create(text_box_params)
+
+        if text_box.valid?
+            render json: {message: "Successfully created text box."}, status: 200
+        else
+            render json: {message: "Failed to create text box, invalid inputs."}, status: 422
+        end
+    end
+
+    def update
+        text_box = TextBox.find(params[:id])
+        render json: text_box.to_json, status: 200
+    end
+
+    def destroy
+        text_box = TextBox.find(params[:id])
+
+        text_box.destroy
+
+        render json: {message: "Successfully deleted text box."}, status: 200
+    end
+
+    private
+    
+    def ordered_list_params
+        params.require(:text_box).permit(:recipe_id, :title, :text_content)
+    end
 end
