73080d26a6267d8465731bae2ac4a7aecb6e4bd3
rmiverson <rmiverson99@gmail.com>
2022-08-11 01:06:44 -0700

    started work on the user serializer, left off on figuring out how to display folders, sub folders, and recipes etc etc.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,19 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id
+
+  def initialize(user)
+    @user = user
+  end
+
+  def serialized_json(token = nil)
+    options = {
+      include: {
+        recipes: {
+          only: {:id, :title}
+        }, 
+        # left off here, try to figure out how to disply folders and their recipes and subfolders
+        # also need to figure out how to create and display recipes with their inputed components
+      }
+    }
+  end
 end
