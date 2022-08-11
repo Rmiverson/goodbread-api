e860bb90c2c9b0a8cb1d7099d64b3cb50c654e2b
Riley Iverson <rmiverson@RileysPC.localdomain>
2022-08-11 00:10:29 -0700

    fixed some formmating in auth controller, and built the user controller loosely

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,19 +3,17 @@ class AuthController < ApplicationController
  
     # for login
     def create
-       user = User.find_by(username: params[:username])
- 
-       # byebug
- 
-       if user && user.authenticate(params[:password])
-          my_token = encode_token({user_id: user.id})
-          render json: UserSerializer.new(user).serialized_json(my_token), status: 200
-       else
-          render json: {error: 'That user could not be found'}, status: 401
-       end
+         user = User.find_by(username: params[:username])
+         
+         if user && user.authenticate(params[:password])
+            my_token = encode_token({user_id: user.id})
+            render json: UserSerializer.new(user).serialized_json(my_token), status: 200
+         else
+            render json: {error: 'That user could not be found'}, status: 401
+         end
     end
  
     def show
-       render json: UserSerializer.new(@user).serialized_json
+         render json: UserSerializer.new(@user).serialized_json
     end
  end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,46 @@
 class UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create]
+
+    # signup
+    def create
+        user = User.create(user_params)
+
+        if user.valid?
+            token = encode_token({user_id: user.id})
+            render json: UserSerializer.new(user).serialized_json(token)
+        else
+            render json: { error: "Invalid inputs"}, status: => 422
+        end
+    end
+
+    def index
+        users = User.all
+
+        render json: UserSerializer.new(users).serialized_json
+    end
+
+    def show
+        user = User.find(params[:id])
+
+        render json: UserSerializer.new(user).serialized_json
+    end
+
+    def update
+        user = User.find(params[:id])
+
+        user.update(user_params)
+
+        render json: UserSerializer.new(user).serialized_json
+    end
+
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+        render json: {message: "User successfully deleted"}, status: 200
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:id)
+    end
 end
