10ff38c1ed751c3a5ecdff87a1da6c609a6e18c7
rmiverson <rmiverson99@gmail.com>
2022-09-01 11:43:28 -0700

    moved component methods into the respective controller methods, validated that create update and destroy works for components

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,7 +3,22 @@ class RecipesController < ApplicationController
         recipe = Recipe.create(recipe_params)
 
         if recipe.valid?
-            Recipe.createComponents(recipe_params[:components], recipe[:id])
+            arr = JSON.parse(recipe_params[:components])
+
+            if !arr.empty?()
+                arr.map do |component|
+                    if component["type"] === "ol"
+                        OrderedList.create(recipe_id: recipe[:id], title: component["title"], list_items: component["list_items"])
+                    elsif component["type"] === "ul"
+                        UnorderedList.create(recipe_id: recipe[:id], title: component["title"], list_items: component["list_items"])
+                    elsif component["type"] === "textbox"
+                        Textbox.create(recipe_id: recipe[:id], title: component["title"], text_content: component["text_content"])
+                    else
+                        raise Exception.new "Failed to determine recipe component type."
+                    end
+                end
+            end
+
             render json: RecipeSerializer.new(recipe).serialized_json
         else
             render json: { error: "Invalid inputs", status: 422}, status: 422
@@ -25,16 +40,62 @@ class RecipesController < ApplicationController
 
     def update
         recipe = Recipe.find(params[:id])
-
         recipe.update(recipe_params)
+        arr = JSON.parse(recipe_params[:components])
+
+        if !arr.empty?()
+            arr.map do |componentParameters|
+                if componentParameters["type"] === "ol"
+                    component = OrderedList.find(componentParameters["id"])
+                    component.update(title: componentParameters["title"], list_items: componentParameters["list_items"])
+                elsif componentParameters["type"] === "ul"
+                    component = UnorderedList.find(componentParameters["id"])
+                    component.update(title: componentParameters["title"], list_items: componentParameters["list_items"])
+                elsif componentParameters["type"] === "textbox"
+                    component = Textbox.find(componentParameters["id"])
+                    component.update(title: componentParameters["title"], text_content: componentParameters["text_content"])
+                else
+                    raise Exception.new "Failed to determine recipe component type."
+                end                
+            end
+        end
 
         render json: RecipeSerializer.new(recipe).serialized_json
     end
 
     def destroy
         recipe = Recipe.find(params[:id])
+
         recipe.destroy
+
         render json: {message: "Recipe successfully deleted"}, status: 200
+    end
+
+    def destroyComponents
+        recipe = Recipe.find(params[:id])
+
+        arr = JSON.parse(recipe_params[:components])
+
+        if !arr.empty?()
+            arr.map do |componentParameters|
+                if componentParameters["type"] === "ol"
+                    component = OrderedList.find(componentParameters["id"])
+                    component.destroy
+                elsif componentParameters["type"] === "ul"
+                    component = UnorderedList.find(componentParameters["id"])
+                    component.destroy
+                elsif componentParameters["type"] === "textbox"
+                    component = Textbox.find(componentParameters["id"])
+                    component.destroy
+                else
+                    raise Exception.new "Failed to determine recipe component type."
+                end                
+            end
+
+            render json: {message: "Recipe compnent(s) successfully deleted"}, status: 200
+        else
+            render json: {message: "No recipe component(s) to delete."}, status: 404
+        end
     end
 
     private
