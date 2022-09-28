579986a6da99c2e6b23a62af8bb79782f33cf8f7
rmiverson <rmiverson99@gmail.com>
2022-09-28 14:44:15 -0700

    put back the old format for the serializer and included meta information and joined it with the returned json, still not ideal since to access the data we have to go into data twice because of how it got formatted

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,8 +1,30 @@
 class RecipeSerializer < ActiveModel::Serializer
   attributes :id, :title, :description
 
-  has_many :textboxes
-  has_many :ordered_lists
-  has_many :unordered_lists
-  has_many :tags
+  def initialize (recipe, meta = {})
+    @recipe = recipe
+  end
+
+  def serialized_json(meta = {})
+    options = {
+      include: {
+        tags: {
+          only: [:id, :label]
+        },
+        ordered_lists: {
+          only: [:id, :title, :list_items, :component_type, :index_order]
+        },
+        unordered_lists: {
+          only: [:id, :title, :list_items, :component_type, :index_order]
+        },
+        textboxes: {
+          only: [:id, :title, :text_content, :component_type, :index_order]
+        }
+      },
+      except: [:created_at, :updated_at]
+    }
+    data = JSON.parse(@recipe.to_json(options))
+    dataWithMeta = {data: data, meta: meta}
+    dataWithMeta.to_json
+  end
 end
