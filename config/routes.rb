fc82ca10838a0730ce3d7edc7a0ed71bd3283902
rmiverson <rmiverson99@gmail.com>
2022-10-03 11:41:26 -0700

    updated routes to be put instead of patch to accomidate frontend requests

--- config/routes.rb
+++ config/routes.rb
@@ -13,20 +13,20 @@ Rails.application.routes.draw do
 
   get '/users/:id/folders', to: 'users#show_folders'
   get '/users/:id/recipes', to: 'users#show_recipes'
-  patch '/users/:id', to: 'users#update'
+  put '/users/:id', to: 'users#update'
   delete '/users/:id', to: 'users#destroy'
 
-  patch '/recipes/:id', to: 'recipes#update'
+  put '/recipes/:id', to: 'recipes#update'
   delete '/recipes/:id', to: 'recipes#destroy'
   delete '/recipes/:id/components', to: 'recipes#destroyComponents'
 
   get '/folders/:id/recipes', to: 'folders#show_recipes'
   get '/folders/:id/sub_folders', to: 'folders#show_sub_folders'
-  patch '/folders/:id', to: 'folders#update'
+  put '/folders/:id', to: 'folders#update'
   delete '/folders/:id', to: 'folders#destroy'
 
   get '/sub_folders/:id/recipes', to: 'sub_folders#show_recipes'
-  patch '/sub_folders/:id', to: 'sub_folders#update'
+  put '/sub_folders/:id', to: 'sub_folders#update'
   delete '/sub_folders/:id', to: 'sub_folders#destroy'
 
   get '/folders_recipes', to: 'folders_recipes#find_by'
@@ -35,9 +35,9 @@ Rails.application.routes.draw do
   get '/recipe_sub_folder_joins', to: 'recipe_sub_folder_joins#find_by'
   delete '/recipe_sub_folder_joins/:id', to: 'recipe_sub_folder_joins#destroy'
 
-  patch '/tags/:id', to: 'tags#update'
+  put '/tags/:id', to: 'tags#update'
   delete '/tags/:id', to: 'tags#destroy'
 
-  patch '/tag_recipe_joins', to: 'tag_recipe_joins#find_by'
+  put '/tag_recipe_joins', to: 'tag_recipe_joins#find_by'
   delete '/tag_recipe_joins/:id', to: 'tag_recipe_joins#destroy'
 end
