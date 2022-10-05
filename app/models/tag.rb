e1a4e0522b1d9da89049d995de6f908e733e174d
rmiverson <rmiverson99@gmail.com>
2022-10-05 11:53:54 -0700

    added a validation for label

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,6 @@
 class Tag < ApplicationRecord
+    validates :label, presence: true, uniqueness: true
+
     has_many :recipes_tags, :dependent => :destroy
     has_many :recipes, through: :recipes_tags, :dependent => :destroy
 end
