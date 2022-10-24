29015262337ac033811c0b028ac45e225e875691
Riley Iverson <rmiverson@RileysPC>
2022-10-24 14:13:32 -0700

    added self joins to folders but still need to remove sub folders and validate everything

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,9 +1,14 @@
 class Folder < ApplicationRecord
     attr_accessor :folder_ids
 
-    has_many :sub_folders, :dependent => :destroy
+    # has_many :sub_folders, :dependent => :destroy
     has_many :folders_recipes, :dependent => :destroy
-    has_many :recipes, through: :folders_recipes, :dependent => :destroy
+    # has_many :recipes, through: :folders_recipes, :dependent => :destroy
+
+    has_many :sub_folders, foreign_key: :child_folder_id, class_name: "FolderToFolderJoin"
+    has_many :sub_folder_recipes, through: :sub_folders, :dependent => :delete_all
+    has_many :parent_folders, foreign_key: :parent_folder_id, class_name: "FolderToFolderJoin"
+    has_many :parent_folder_recipes, through: :parent_folders, :dependent => :delete_all
 
     belongs_to :user
 
