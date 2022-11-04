4410a768f3e68aa4bcbe254567bc4cc51efd804a
rmiverson <rmiverson99@gmail.com>
2022-11-04 14:45:01 -0700

    fixed some things with the folder to folder join associations which were preventing folders from being deleted

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -2,10 +2,10 @@ class Folder < ApplicationRecord
     has_many :folders_recipes, :dependent => :destroy
     has_many :recipes, through: :folders_recipes
 
-    has_many :sub_folders, foreign_key: :child_folder_id, class_name: "FolderToFolderJoin"
-    has_many :sub_folder_recipes, through: :sub_folders, :dependent => :delete_all
+    has_many :child_folders, foreign_key: :child_folder_id, class_name: "FolderToFolderJoin"
+    has_many :child_folder_recipes, through: :child_folders, :source => :child_folder, :dependent => :delete_all
     has_many :parent_folders, foreign_key: :parent_folder_id, class_name: "FolderToFolderJoin"
-    has_many :parent_folder_recipes, through: :parent_folders, :dependent => :delete_all
+    has_many :parent_folder_recipes, through: :parent_folders, :source => :parent_folder, :dependent => :delete_all
 
     belongs_to :user
 
--- app/models/folder_to_folder_join.rb
+++ app/models/folder_to_folder_join.rb
@@ -1,4 +1,4 @@
 class FolderToFolderJoin < ApplicationRecord
-    belongs_to :sub_folder, class_name: "Folder"
+    belongs_to :child_folder, class_name: "Folder"
     belongs_to :parent_folder, class_name: "Folder"
 end
