211b338e0a9c05be0d0b0a84990d8f836adfb1b8
rmiverson <rmiverson99@gmail.com>
2023-01-04 12:10:34 -0800

    started work on an error handler for unprocessable entity but couldnt figure out what active record calls it

--- lib/error/error_handler.rb
+++ lib/error/error_handler.rb
@@ -5,6 +5,12 @@ module Error
                 rescue_from ActiveRecord::RecordNotFound do |e|
                     respond(:record_not_found, 404, e.to_s)
                 end
+                
+                # doesnt work, activeRecord::unprocessableentity isnt a thing, need to find out what it really is
+                # rescue_from ActiveRecord::UnprocessableEntity do |e|
+                #     respond(:unprocessable_entity, 422, e.to_s)
+                # end
+
                 rescue_from StandardError do |e|
                     respond(:standard_error, 500, e.to_s)
                 end
