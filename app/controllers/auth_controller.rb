8bf889aaa5b7566db8b80c6431ce72718f10085d
rmiverson <rmiverson99@gmail.com>
2023-01-05 11:59:34 -0800

    removed the require for json_web_token, implmented hard params and fixed how find by and auth gets into the params, also reformatted and gave login better error handling

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,18 +1,20 @@
-require './lib/json_web_token'
-
 class AuthController < ApplicationController
    before_action :authorize_request, except: :login
 
    def login
-      @user = User.find_by(username: params[:user][:username])
+      @user = User.find_by(username: login_params[:username])
 
-      if @user&.authenticate(params[:user][:password])
+      if @user&.authenticate(login_params[:password])
          token = JsonWebToken.encode(user_id: @user.id)
+
          time = Time.now + 24.hours.to_i
          time_milli = time.to_f * 1000
+
          render json: UserSerializer.new(@user).serialized_json({token: token, exp: time_milli})
       else
-         render json: { error: 'unauthorized'}, status: :unauthorized
+         render json: {
+            error: 'Unauthorized Login.'
+         }, status: :unauthorized
       end
    end
 
