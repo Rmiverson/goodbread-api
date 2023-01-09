00c6fa9caf0ff8d0662f348d7e958c71b5fc0927
rmiverson <rmiverson99@gmail.com>
2023-01-09 10:20:22 -0800

    Gave tags better error handling.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,41 +1,79 @@
 class TagsController < ApplicationController
     before_action :authorize_request
     def create
-        tag = Tag.create(tag_params)
+        @tag = Tag.create(tag_params)
 
-        if tag.valid?
-            render json: {message: "Tag successfully created."}, status: 201
+        if @tag
+            render json: {
+                message: "Tag successfully created."
+            }, status: :ok
         else
-            render json: {message: "Failed to create tag, invalid inputs."}, status: 422
+            render json: {
+                error: "Failed to create tag."
+            }, status: :bad_request
         end
     end
 
     def index
-        tags = Tag.all
+        @tags = Tag.all
 
-        render json: tags.to_json, status: 200
+        if @tags
+            render json: @tags.to_json
+        else
+            render json: {
+                error: "Could not get all tags."
+            }, status: :internal_server_error
+        end
     end
 
     def show
-        tag = Tag.find(params[:id])
+        @tag = Tag.find(params[:id])
 
-        render json: tag.to_json, status: 200
+        if @tag
+            render json: @tag.to_json
+        else
+            render json: {
+                error: "Could not find tag with id of #{@tag[:id]}."
+            }, status: :not_found
+        end
     end
 
     def update
-        tag = Tag.find(params[:id])
+        @tag = Tag.find(params[:id])
 
-        tag.update(tag_params)
+        if @tag
+            unless @tag.update(tag_params)
+                render json: {
+                    error: "Could not update tag with id of #{@tag[:id]}."
+                }, status: :bad_request
+            end
 
-        render json: tag.to_json, status: 200
+            render json: @tag.to_json
+        else
+            render json: {
+                error: "Could not find tag with id of #{@tag[:id]}."
+            }, status: :not_found
+        end
     end
 
     def destroy
-        tag = Tag.find(params[:id])
+        @tag = Tag.find(params[:id])
         
-        tag.destroy
+        if @tag
+            unless @tag.destroy
+                render json: {
+                    error: "Could not delete tag with id of #{@tag[:id]}."
+                }
+            end
 
-        render json: {message: "Tag successfully deleted."}, status: 200
+            render json: {
+                message: "Tag successfully deleted."
+            }, status: :ok
+        else
+            render json: {
+                error: "Could not find tag with id of #{@tag[:id]}."
+            }, status: :not_found
+        end
     end
 
     private
