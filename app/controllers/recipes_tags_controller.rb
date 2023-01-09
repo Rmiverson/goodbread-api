44f22d19f9bfcfb42a23d873640df7670ea48550
rmiverson <rmiverson99@gmail.com>
2023-01-09 10:26:36 -0800

    Gave recipe tag join controller error handling.

--- app/controllers/recipes_tags_controller.rb
+++ app/controllers/recipes_tags_controller.rb
@@ -1,32 +1,48 @@
 class RecipesTagsController < ApplicationController
     before_action :authorize_request
     def create
-        recipe_tag = RecipesTag.create(join_params)
+        @recipe_tag = RecipesTag.create(join_params)
 
-        if (recipe_tag.valid?)
-            render json: { message: "Successfully joined Tag and Recipe."}, status: 201
+        if @recipe_tag
+            render json: {
+                message: "Successfully joined Tag and Recipe."
+            }, status: :ok
         else
-            render json: { message: "Failed to join Recipe and Tag."}, status: 400
+            render json: {
+                error: "Failed to join Recipe and Tag."
+            }, status: :bad_request
         end
     end
 
     def find_by
-        recipe_tag = RecipesTag.find_by recipe_id: join_params[:recipe_id], tag_id: join_params[:tag_id]
+        @recipe_tag = RecipesTag.find_by recipe_id: join_params[:recipe_id], tag_id: join_params[:tag_id]
         
-        if recipe_tag.nil?
-            render json: { message: "Tag Recipe join could not be found."}, status: 404
+        if @recipe_tag.nil?
+            render json: {
+                error: "Could not find recipe tag relation."
+            }, status: :not_found
         else
-            render json: recipe_tag.to_json, status: 200
+            render json: @recipe_tag.to_json
         end
     end
 
     def destroy
-        recipe_tag = RecipesTag.find(params[:id])
+        @recipe_tag = RecipesTag.find(params[:id])
 
-        recipe_tag.destroy
+        if @recipe_tag
+            unless @recipe_tag.destroy
+                render json: {
+                    error: "Could not delete recipe tag relation."
+                }, status: :internal_server_error
+            end
 
-        render json: { message: "Join successfully deleted."}, status: 200
+            render json: { message: "Join successfully deleted."}
+        else
+            render json: {
+                error: "Could not find recipe tag relation."
+            }, status: :not_found
         end
+    end
 
     private
 
