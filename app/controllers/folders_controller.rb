d72b03a5d22bee458eda1ad19b0a8d7f72fac46e
rmiverson <rmiverson99@gmail.com>
2023-01-09 10:01:11 -0800

    put error handling for create and show recipes

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -2,21 +2,34 @@ class FoldersController < ApplicationController
     before_action :authorize_request
 
     def create
-        folder = Folder.create(folder_params)     
+        @folder = Folder.create(folder_params)     
         
-        if folder.valid?
+        if @folder.valid?
             render json: FolderSerializer.new(folder).serialized_json
         else
-            render json: { message: "Failed to create folder, invalid inputs" }, status: 422 
+            render json: {
+                Error: "Failed to create folder, invalid inputs"
+            }, status: :bad_request
         end
     end
 
     def show_recipes
-        folder = Folder.find(params[:id])
-        
-        recipes = folder.recipes.page(params[:page]).per(15)
+        @folder = Folder.find(params[:id])
+        @recipes = folder.recipes.page(params[:page]).per(15)
 
-        render json: RecipeSerializer.new(recipes).serialized_json(meta_attributes(recipes))
+        if @folder
+            if @recipes
+                render json: RecipeSerializer.new(recipes).serialized_json(meta_attributes(recipes))
+            else
+                render json: {
+                    error: "Could not get recipes from folder with id of #{@folder[:id]}"
+                }, status: :internal_server_error
+            end
+        else
+            render json: {
+                error: "Could not find folder with id of #{@folder[:id]}"
+            }, status: :not_found
+        end
     end
 
     def show
