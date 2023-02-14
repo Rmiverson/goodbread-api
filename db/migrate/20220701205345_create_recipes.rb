d6d29824c1b4cbee8445b6c2deb56a17fa2990a4
rmiverson <rmiverson99@gmail.com>
2023-02-14 12:08:21 -0800

    removed components migration and added a column in recipes to take bodyText from the lexical framework on the frontend

--- db/migrate/20220701205345_create_recipes.rb
+++ db/migrate/20220701205345_create_recipes.rb
@@ -4,6 +4,7 @@ class CreateRecipes < ActiveRecord::Migration[7.0]
       t.references :user, null: false, foreign_key: true
       t.string :title, null: false
       t.text :description
+      t.text :bodyText
       
       t.timestamps null: false
     end
